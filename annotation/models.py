f0dbb9455b39236aa2cb1da49946c763fc5909ab
abgd29 <anne.beigeaud2@gmail.com>
2025-01-09 22:22:00 +0100

    Creation application annotation

--- annotation/models.py
+++ annotation/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+
+# Create your models here.
+from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib import admin
