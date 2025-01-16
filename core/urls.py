839ec9f6285a43a9cbee4702d718ca21dbb78837
Youna <youna.maillie@etu-upsaclay.fr>
2025-01-16 18:20:19 +0100

    update_16_01_interface

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("inscription/", views.inscription, name="inscription"),
     path("connexion/", views.connexion, name="connexion"),
-    path("contacts/", views.contact, name="contacts"),
+    path("contacts/", views.contacts, name="contacts"),
     path("visualisation/", views.visualisation, name = "visualisation"),
     path("database/", views.database, name="genome_database")
 ]
--- core/views.py
+++ core/views.py
@@ -3,13 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from .form import CustomUserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import Genome
 
 # Page d'accueil
 def home(request):
     return render(request,"core/home.html")
 
 # Contacts
-def contact(request):
+def contacts(request):
     return render(request,"core/contacts.html")
 
 def database(request):
@@ -40,3 +41,34 @@ def connexion(request):
         else:
             return messages.error(request, "Nom d'utilisateur ou mot de passe incorrect")
     return render(request, 'core/connexion.html')
+
+def database_view(request):
+    user_request = request.GET.get("user_request", "")
+    sequence_type = request.GET.get("sequence_type", "")
+
+    if not user_request and not sequence_type:
+        results = Genome.objects.all()
+    else:
+        results = Genome.objects.filter(
+            organism__icontains=user_request,
+            genome_type__icontains=sequence_type
+        )
+        
+        context = {"results": results}
+        return render(request,"database.html", context)
+
+# def database_search(request):
+#     query = request.GET.get("user_request", "")
+#     seq_type = request.GET.get("sequence_type", "")
+
+#     listing_of_results = Genome.objects.all()
+
+#     if query:
+#         listing_of_results = listing_of_results.filter(
+#             organism__icontains=query) | listing_of_results.filter(
+#                 sequence_id__icontains=query) | listing_of_results.filter(genome_organism_icontains=query)
+#     if seq_type:
+#         if seq_type == "DNA" or seq_type == "RNA":
+#             listing_of_results = listing_of_results.filter(genome__genome_type=seq_type)
+    
+#     return render(request, "database.html", {"sequences": listing_of_results})
