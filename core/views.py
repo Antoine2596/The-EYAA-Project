9ac05812fa4afbeb4d5b09750885e0752c903347
abgd29 <anne.beigeaud2@gmail.com>
2025-02-08 12:51:27 +0100

    correction typo database

--- core/views.py
+++ core/views.py
@@ -261,7 +261,10 @@ def database_view(request):
                 sequences = sequences.filter(sequence_brin__iexact=Brin)
             if sequence_type:
                 sequences = sequences.filter(selected_type_iexact=sequence_type)
-            
+            if is_annotated:
+                sequences = sequences.filter(
+                    Q(sequence_status__iexact="Validated") | Q(sequence_status__iexact="Awaiting validation")
+                            )
 
         combined_results = (
     [{"obj": obj, "type": "Genome"} for obj in genomes] if genomes else []
@@ -541,6 +544,10 @@ def extraction_file(request):
             sequences = sequences.filter(sequence_brin__iexact=Brin)
         if sequence_type:
             sequences = sequences.filter(selected_type_iexact=sequence_type)
+        if is_annotated:
+                sequences = sequences.filter(
+                    Q(sequence_status__iexact="Validated") | Q(sequence_status__iexact="Awaiting validation")
+                            )
 
 
     # 2- Récupérer les champs sélectionnés
